e29597b2e2f5ef78e980642c6c9e7352c259d76a
Vaskebjorn <99696313+raccoonvaskebjorn@users.noreply.github.com>
2024-05-18 13:29:45 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ document.getElementById("main-action-button").onclick = function(){
     document.getElementById("products").scrollIntoView({behavior: "smooth"});
 }
 // PROCESS HEADER LINKS
-const links = document.querySelectorAll(".menu-item > a");
+const links = document.querySelectorAll(".menu_item > a");
 for(let i = 0; i < links.length; i++){
     links[i].onclick = function(){
         document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: "smooth"});
